ffbcda43a92c76b7dd8b0c8d25320aac1db1be20
Evandru <evandro_remolli.t@outlook.com>
2023-05-03 16:29:06 -0300

    Movies search model

--- MovieModel.cs
+++ MovieModel.cs
@@ -10,9 +10,22 @@ namespace SimplesConsumoAPI
     {
         public string Title { get; set; }
         public string Plot { get; set; }
-        public string Year { get; set; }
+        public int Year { get; set; }
         public string Actors { get; set; }
         public string Genre { get; set; }
-
+        public string Poster { get; set; }
+    }
+    public class SearchResponseModel
+    {
+        public List<MovieBySearch> Search { get; set; }
+        public int totalResults { get; set; }
+        public bool Response { get; set; }
+    }
+    public class MovieBySearch
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
     }
 }

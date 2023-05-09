afce6ce73fe1f455b891769537f4c8374daf9575
Evandru <evandro_remolli.t@outlook.com>
2023-05-09 11:02:42 -0300

    Redução de responsabilidade - Views não instancia a API

--- Views.cs
+++ Views.cs
@@ -6,22 +6,44 @@ using System.Threading.Tasks;
 
 namespace SimplesConsumoAPI
 {
-    public class Views
+    public static class Views
     {
-        API api;
-        public Views()
+        public static void MovieView(MovieModel movie)
         {
-            api = new API();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Título: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(movie.Title);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Descrição: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(movie.Plot);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Released: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(movie.Year);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Genre: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(movie.Genre);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Starring: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(movie.Actors);
+
+            Console.ResetColor();
         }
-        public async Task MovieView()
+
+        public static void SearchResponseView(SearchResponseModel searchResponse)
         {
-            while (true)
+            foreach (var movie in searchResponse.Search)
             {
-                Console.Write("Digite o título do filme: ");
-
-                var title = Console.ReadLine();
-                var movie = await api.GetMovie(title);
-
                 Console.WriteLine();
 
                 Console.ForegroundColor = ConsoleColor.Magenta;
@@ -30,68 +52,26 @@ namespace SimplesConsumoAPI
                 Console.WriteLine(movie.Title);
 
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Descrição: ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(movie.Plot);
-
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Released: ");
+                Console.Write("Lançado em ");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(movie.Year);
 
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Genre: ");
+                Console.Write("Tipo: ");
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(movie.Genre);
-
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Starring: ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(movie.Actors);
+                Console.WriteLine(movie.Type);
 
                 Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine(new string('-', 20));
             }
-        }
 
-        public async Task SearchResponseView()
-        {
-            while (true)
-            {
-                Console.Write("Digite um título: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Total de resultados: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(searchResponse.totalResults);
 
-                var title = Console.ReadLine();
-                var searchResponse = await api.GetSearchResponse(title);
-
-                foreach (var movie in searchResponse.Search) 
-                { 
-                    Console.WriteLine();
-
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Título: ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(movie.Title);
-
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Lançado em ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(movie.Year);
-
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Tipo: ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(movie.Type);
-
-                    Console.ResetColor();
-                    Console.WriteLine(new string('-', 20));
-                }
-
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.Write("Total de resultados: ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(searchResponse.totalResults);
-
-                Console.ResetColor();
-            }
+            Console.ResetColor();
         }
     }
 }

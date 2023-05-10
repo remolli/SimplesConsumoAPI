95bf7cb59b7042df30e4312b2dfa3a29dc042c8c
Evandru <evandro_remolli.t@outlook.com>
2023-05-10 12:48:21 -0300

    Little Design

--- Views.cs
+++ Views.cs
@@ -13,32 +13,32 @@ namespace SimplesConsumoAPI
             Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Título: ");
+            Console.Write(" | Título: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Title);
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Descrição: ");
+            Console.Write(" | Descrição: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Plot);
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Released: ");
+            Console.Write(" | Released: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Year);
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Genre: ");
+            Console.Write(" | Genre: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Genre);
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Starring: ");
+            Console.Write(" | Starring: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Actors);
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Poster: ");
+            Console.Write(" | Poster: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(movie.Poster);
 
@@ -54,17 +54,17 @@ namespace SimplesConsumoAPI
                     Console.WriteLine();
 
                     Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Título: ");
+                    Console.Write(" | Título: ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(movie.Title);
 
                     Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Lançado em ");
+                    Console.Write(" | Lançado em ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(movie.Year);
 
                     Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Tipo: ");
+                    Console.Write(" | Tipo: ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(movie.Type);
 
@@ -75,7 +75,7 @@ namespace SimplesConsumoAPI
             }
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Total de resultados: ");
+            Console.Write(" | Total de resultados: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(searchResponse.totalResults);
 
